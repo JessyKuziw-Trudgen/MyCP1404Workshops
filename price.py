d17d75110132a31624fb98bacfb94b81d1a2f313
Jessy Kuziw <trudgen@hotmail.com>
2016-03-23 12:55:24 +1000

    added function in price.py

---
+++ price.py
@@ -0,0 +1,29 @@
+import random
+MAX_INCREASE = 0.175  # 10%
+MAX_DECREASE = 0.05  # 5%
+MIN_PRICE = 1.0
+MAX_PRICE = 100.0
+INITIAL_PRICE = 10.0
+price = INITIAL_PRICE
+day = 0
+
+
+def function_currency(value):
+    return "${:,.2f} ".format(value)
+
+print("Starting price {0}".format(function_currency(price)))
+while MIN_PRICE <= price <= MAX_PRICE:
+    day += 1
+    priceChange = 0
+    # generate a random integer of 1 or 2
+    # if it's 1, the price increases, otherwise it decreases
+    if random.randint(1, 2) == 1:
+        # generate a random floating-point number
+        # between 0 and MAX_INCREASE
+        priceChange = random.uniform(0, MAX_INCREASE)
+    else:
+        # generate a random floating-point number
+        # between negative MAX_INCREASE and 0
+        priceChange = random.uniform(-MAX_DECREASE, 0)
+    price *= (1 + priceChange)
+    print("current day {1} {0} ".format(function_currency(price), day))
